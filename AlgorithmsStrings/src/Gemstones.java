5500cd03ad5113e95cbc167d378d9e8faf6723ca
ryan tan jun ming <ryantanjunming@gmail.com>
2015-11-30 14:58:03 +0800

    Adding - Gemstones -  Anagram
    Going to try and remove:
    - All in .meta
    - All with .class

--- AlgorithmsStrings/src/Gemstones.java
+++ AlgorithmsStrings/src/Gemstones.java
@@ -1,4 +1,4 @@
-import java.util.Collection;
+
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -12,18 +12,21 @@ public class Gemstones {
 		int N = in.nextInt();
 		int T = N;
 		Hashtable<Character, Integer> hash = new Hashtable<Character, Integer>();
+		String abc = "abcdefghijklmnopqrstuvwxyz";
+		
+		for(int i=0; i<abc.length(); i++){
+			hash.put(abc.charAt(i), 0);
+		}
+		
 		
 		while(T > 0){
-			String rock = in.next();
+			String rock = in.next().toLowerCase().trim();
 			LinkedList<Character> dump = new LinkedList<Character>();
 			
 			for(int i=0; i<rock.length(); i++){
 				char element = rock.charAt(i);
-				if(hash.containsKey(element) && dump.contains(element) == false){
+				if(!dump.contains(element)){
 					hash.put(element, hash.get(element) + 1);
-				}else{
-					hash.put(element, 1);
-					
 				}
 				dump.add(element);
 			}
---
+++ README.md
@@ -0,0 +1,40 @@
+### Introduction
+
+All My Hacker Rank code to date, practice space for algorithms bashing.
+
+Algorithms -> Warm Up:
+
+* A Very Big Sum
+* Diagonal Difference
+* Plus Minus
+* Simple Array Sum
+* Solve Me Second
+* Staircase
+* Time Conversion
+
+Algorithms -> Implementation:
+
+* ACM ICPC Team
+* Angry Professor
+* Caesar Cipher
+* Cavity Map
+* Chocolate Feast
+* Cut the Sticks
+* Encryption
+* Extra Long Factorials
+* Find Digits
+* Library Fine
+* Service Lane
+* Sherlock and The Beast
+* Taum And Bday
+* The Grid Search
+* The Time in Words
+* Utopian Tree
+
+Algorithms -> Strings
+
+* Alternating Characters
+* Funny Strings
+* Pangrams
+* Gemstones
+
