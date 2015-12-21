58749ba6beb33011138f3b11b01a91a05247a38e
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2015-12-22 01:59:28 +0800

    Finished: - SherlockAndArray.java - IceCreamParlor.java

---
+++ AlgorithmsSearch/src/IceCreamParlor.java
@@ -0,0 +1,38 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class IceCreamParlor {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		
+		while(T>0){
+			int M = in.nextInt();
+			int N = in.nextInt();
+			Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
+			
+			
+			for(int idx=0; idx<N; idx++){
+				int num = in.nextInt();
+				//find match first
+				int toFindNum = M - num;
+				Integer pair = null;
+				if((pair = ht.get(toFindNum)) != null){
+					int foundDex = idx+1;
+					int foundPair = pair+1;
+					System.out.println(foundPair + " " + foundDex);
+				}else{
+					ht.put(num, idx);
+				}
+			}
+			
+			
+			
+			T--;
+		}
+
+	}
+
+}
--- AlgorithmsSearch/src/SherlockAndArray.java
+++ AlgorithmsSearch/src/SherlockAndArray.java
@@ -1,8 +1,49 @@
+import java.util.Scanner;
 
 public class SherlockAndArray {
 
 	
 	public static void main(String[] args){
-		
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		while(T>0){
+			int N = in.nextInt();
+			int[] arr = new int[N];
+			int[] fromLeft = new int[N];
+			int[] fromRight = new int[N];
+			
+			int leftTotal = 0;
+			for(int i=0; i<N; i++){
+				int num = in.nextInt();
+				leftTotal += num;
+				arr[i] = num;
+				fromLeft[i] = leftTotal;
+			}
+			
+			int rightTotal = 0;
+			for(int i=N-1; i>=0; i--){
+				rightTotal += arr[i];
+				fromRight[i] = rightTotal;
+			}
+			
+			boolean found = false;
+			for(int i=0; i<N; i++){
+				if(fromLeft[i] == fromRight[i]){
+					found = true;
+					break;
+				}
+			}
+			
+			if(found == true){
+				System.out.println("YES");
+			}else{
+				System.out.println("NO");
+			}
+			
+			
+			T--;
+		}
 	}
+	
+
 }
