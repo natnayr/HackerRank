51939612b133f4a74fc25067436565ab4a809289
ryan tan jun ming <ryantanjunming@gmail.com>
2015-12-29 10:11:01 +0800

    finished MaximiseSum - had to cheat...

--- AlgorithmsSearch/src/MaximiseSum.java
+++ AlgorithmsSearch/src/MaximiseSum.java
@@ -2,45 +2,42 @@ import java.util.Scanner;
 
 public class MaximiseSum {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		long T = in.nextInt();
-		
-		while(T>0){
-			
+
+		while (T > 0) {
+
 			int N = in.nextInt();
-			long M = in.nextInt();
-			
-			long[] arr = new long[N];
-//			int[][] searchBox = new int[N][N];
-			long max = 0;
-			
-			
-			for(int i=0; i<N; i++){
+			int M = in.nextInt();
+
+			int[] arr = new int[N];
+			int[] prefix = new int[N];
+
+			for (int i = 0; i < N; i++) {
 				arr[i] = in.nextInt();
 			}
-			
-			boolean found = false;
-			long x=1;
-			for(int j=0; j<N && found == false; j++){
-				for(int i = 0; i<N && i<x && found == false; i++){
-					long modSum = (arr[i] + arr[j]) % M;
-					if(max < modSum){
-						max = modSum;
-						if(modSum == M-1){
-							found = true;
-						}
-					}
-				}
-				x++;
+
+			// prefix array, modulo arithmic (A+B)%M = (A%M + B%M)%M
+			int curr = 0;
+			for (int i = 0; i < N; i++) {
+				curr = (arr[i] % M + curr) % M;
+				prefix[i] = curr;
 			}
-			
-			System.out.println(max);
-			
+
+			int ret = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = i - 1; j >= 0; j--) {
+					ret = Math.max(ret, (prefix[i] - prefix[j] + M) % M);
+				}
+				ret = Math.max(ret, prefix[i]);
+			}
+
+			System.out.println(ret);
+
 			T--;
 		}
-		
+
 		in.close();
 	}
-	
 }
