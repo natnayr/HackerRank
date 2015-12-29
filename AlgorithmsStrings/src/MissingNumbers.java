2c61901c446bf7cf366565c904f9b9a112a2d76a
ryan tan jun ming <ryantanjunming@gmail.com>
2015-12-29 14:37:16 +0800

    Maximum Sum not working, gonna not care about it.
    Finished Missing Numbers
    
    Stared on Connected Cell in a Grid

--- AlgorithmsStrings/src/MissingNumbers.java
+++ AlgorithmsStrings/src/MissingNumbers.java
@@ -1,11 +1,47 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class MissingNumbers {
 
 	public static void main(String[] args) {
 
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> hmListA = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> hmListB = new HashMap<Integer, Integer>();
 
+		Scanner in = new Scanner(System.in);
+
+		int NA = in.nextInt();
+		for (int i = 0; i < NA; i++) {
+			int num = in.nextInt();
+			int count = hmListA.containsKey(num) ? hmListA.get(num) : 0;
+			hmListA.put(num, count + 1);
+		}
+
+		int NB = in.nextInt();
+		for (int i = 0; i < NB; i++) {
+			int num = in.nextInt();
+			int count = hmListB.containsKey(num) ? hmListB.get(num) : 0;
+			hmListB.put(num, count + 1);
+		}
+
+		TreeSet<Integer> sortedSet = new TreeSet<Integer>();
+
+		Iterator<Integer> hmbit = hmListB.keySet().iterator();
+		while (hmbit.hasNext()) {
+			int bNum = hmbit.next();
+			if (hmListA.containsKey(bNum) == false) {
+				sortedSet.add(bNum);
+			} else {
+				if (hmListA.get(bNum) < hmListB.get(bNum)) {
+					sortedSet.add(bNum);
+				}
+			}
+		}
+
+		for (int item : sortedSet) {
+			System.out.print(item + " ");
+		}
 	}
-
 }
