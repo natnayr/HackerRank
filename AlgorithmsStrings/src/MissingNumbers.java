91f5cb3a2ec8addb48659b1181406c293a1de656
ryan tan jun ming <ryantanjunming@gmail.com>
2015-12-29 10:38:50 +0800

    Missing Numbers exercise

---
+++ AlgorithmsStrings/src/MissingNumbers.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class MissingNumbers {
+
+	public static void main(String[] args) {
+
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+
+	}
+
+}
