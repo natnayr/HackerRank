3520c4a4512295e67026d999b6f66659f36fc4f5
ryan tan jun ming <ryantanjunming@gmail.com>
2015-12-31 12:08:44 +0800

    ConnectedCellInAGrid.java

--- AlgorithmsStrings/src/ConnectedCellInAGrid.java
+++ AlgorithmsStrings/src/ConnectedCellInAGrid.java
@@ -1,9 +1,74 @@
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class ConnectedCellInAGrid {
 
+	private static int[][] cell;
+	private static boolean[][] searchedMap;
+	private static int M;
+	private static int N;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
 
+		M = in.nextInt();
+		N = in.nextInt();
+
+		cell = new int[M][N];
+		searchedMap = new boolean[M][N];
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				cell[i][j] = in.nextInt();
+				searchedMap[i][j] = false;
+			}
+		}
+
+		int maxCount = 0;
+		LinkedList<Pair> pairList = new LinkedList<Pair>();
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+
+				if (searchedMap[i][j] == false && cell[i][j] == 1) {
+					int count = 0;
+					pairList.add(new Pair(i, j));
+
+					while (pairList.size() > 0) {
+						Pair cur = pairList.pop();
+						int x = cur.x;
+						int y = cur.y;
+						if(){
+						
+							searchedMap[x][y] = true;
+							count++;
+						}
+						if (cell[x + 1][y] == 1)
+							pairList.add(new Pair(x + 1, y));
+						if (cell[x - 1][y] == 1)
+							pairList.add(new Pair(x - 1, y));
+						if (cell[x][y + 1] == 1)
+							pairList.add(new Pair(x, y + 1));
+						if (cell[x][y - 1] == 1)
+							pairList.add(new Pair(x, y - 1));
+
+					}
+
+					if (maxCount < count)
+						maxCount = count;
+				}
+			}
+		}
+
+		System.out.println(maxCount);
 	}
+}
 
+class Pair {
+	public int x;
+	public int y;
+
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
