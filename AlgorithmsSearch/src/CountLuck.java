89fc8b07b9d2cc1eeba2590601aec3f422dc47e1
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2016-01-02 20:35:46 +0800

    today

--- AlgorithmsSearch/src/CountLuck.java
+++ AlgorithmsSearch/src/CountLuck.java
@@ -1,8 +1,51 @@
+import java.util.Scanner;
 
 public class CountLuck {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+
+		while (T > 0) {
+			int N = in.nextInt();
+			int M = in.nextInt();
+
+			char[][] map = new char[N][M];
+			int stX = -1, stY = -1, endX = -1, endY = -1;
+
+			for (int i = 0; i < N; i++) {
+				String line = in.next();
+				for (int j = 0; j < M; j++) {
+					map[i][j] = line.charAt(j);
+
+					if (map[i][j] == '*') {
+						endX = i;
+						endY = j;
+					}
+
+					if (map[i][j] == 'M') {
+						stX = i;
+						stY = j;
+					}
+				}
+			}
+
+			if (stX == -1 || stY == -1 || endX == -1 || endY == -1) {
+				throw new NullPointerException();
+			}
+
+			int K = in.nextInt();
+			boolean found = false;
+			int curX = stX, curY = stY;
+
+			while (found == false) {
+
+			}
+
+			T--;
+		}
+
+		in.close();
 
 	}
 
