3b6c7a4e8c5f48f8a9c2af2ed8c407a21edb4ee7
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2016-01-13 00:00:25 +0800

    SnakesAndLadders - almost solved... sigh.. CoinChaneProblem - have to understand the DP, solution??

---
+++ AlgorithmsDynamicProgramming/src/TheCoinChangeProblem.java
@@ -0,0 +1,9 @@
+
+public class TheCoinChangeProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- AlgorithmsGraphTheory/src/SnakesAndLaddersTheQuickestWayUp.java
+++ AlgorithmsGraphTheory/src/SnakesAndLaddersTheQuickestWayUp.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import sun.security.provider.certpath.Vertex;
-
 public class SnakesAndLaddersTheQuickestWayUp {
 
 	public static void main(String[] args) {
@@ -33,53 +31,42 @@ public class SnakesAndLaddersTheQuickestWayUp {
 				int start = in.nextInt();
 				int end = in.nextInt();
 				board[start - 1].moveTo = end - 1;
-				board[end - 1].isExit = true;
+
 				board[end - 1].moveFrom = start - 1;
 			}
 
 			int i = 0;
 			// hit last square and escape
-
+			int sixCount = 0;
+			int curRoll = 1;
 			while (i < 100) {
 
-				if (board[i].moveTo != -1) {
-
-					if (board[i].moveTo > i) {
-						// ladder
-						int minRoll = Math.min(board[i].getRolls(), board[board[i].moveTo].getRolls());
-						board[board[i].moveTo].setRolls(minRoll);
-					} else {
-						// snake
-						if (board[i].getRolls() < board[board[i].moveTo].getRolls()) {
-							board[board[i].moveTo].setRolls(board[i].getRolls());
-							i = board[i].moveTo;
-						}
-
-					}
-				} else {
-					// normal square
-					if ((i - 1) >= 0 && board[i - 1].isExit == true) {
-						board[i].setRolls(board[i - 1].getRolls() + 1);
-
-					} else if ((i - 6) >= 0 && board[i].isExit == false) {
-						int setRoll = board[i - 1].getRolls();
-						boolean plusOne = true;
-						for (int j = i - 1; j >= 0 && j >= (i - 6); j--) {
-							if (setRoll != board[j].getRolls()) {
-								plusOne = false;
-								break;
-							}
-						}
-
-						if (plusOne) {
-							board[i].setRolls(setRoll + 1);
-						} else {
-							board[i].setRolls(setRoll);
-						}
+				// ladder
+				if (board[i].moveTo > i && board[i].moveTo != -1) {
+					int lEndIdx = board[i].moveTo;
+					if (board[lEndIdx].getRolls() > board[i].getRolls()) {
+						board[lEndIdx].setRolls(board[i].getRolls());
+						changeBoardTillEnd(board, lEndIdx);
 					}
 				}
 
+				// snake
+				if (board[i].moveTo < i && board[i].moveTo != -1) {
+					int snakeEndIdx = board[i].moveTo;
+					if (board[snakeEndIdx].getRolls() > board[i].getRolls()) {
+						board[snakeEndIdx].setRolls(board[i].getRolls());
+						changeBoardTillEnd(board, snakeEndIdx);
+					}
+				}
+
+				sixCount++;
 				i++;
+
+				// set rolls
+				if (sixCount > 6) {
+					sixCount = 0;
+					curRoll++;
+				}
 			}
 
 			System.out.println(board[99].getRolls());
@@ -89,8 +76,51 @@ public class SnakesAndLaddersTheQuickestWayUp {
 		in.close();
 	}
 
-	public static void findPath(Vertex Source) {
+	public static void changeBoardTillEnd(Square[] board, int i) {
 
+		int curRoll = board[i].getRolls() + 1; // next roll to move
+		i += 1;
+		int sixCount = 0;
+
+		while (i < 100) {
+
+			if (curRoll < board[i].getRolls()) {
+				board[i].setRolls(curRoll);
+			}
+
+			// ladder
+			if (board[i].moveTo > i && board[i].moveTo != -1) {
+				// set ladder if less and move through
+				int ladderIdx = board[i].moveTo;
+				if (board[i].getRolls() < board[ladderIdx].getRolls()) {
+					board[ladderIdx].setRolls(board[i].getRolls());
+					i = ladderIdx; // continue from ladder exit
+					sixCount = 0;
+					curRoll++;
+				}
+			}
+
+			// snake
+			if (board[i].moveTo < i && board[i].moveTo != -1) {
+				// set snake if less and move through
+				int snakeIdx = board[i].moveTo;
+				if (board[i].getRolls() < board[snakeIdx].getRolls()) {
+					board[snakeIdx].setRolls(board[i].getRolls());
+					i = snakeIdx;
+					sixCount = 0;
+					curRoll++;
+				}
+			}
+
+			sixCount++;
+			i++;
+
+			// set rolls
+			if (sixCount > 5) {
+				sixCount = 0;
+				curRoll++;
+			}
+		}
 	}
 }
 
