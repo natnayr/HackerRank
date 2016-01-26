15f3c397f5adde4659ff38484764b89a48a40479
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2016-01-26 12:55:25 +0800

    Still failing Test 11 in Cutting Boards

--- AlgorithmsGreedy/src/CuttingBoards.java
+++ AlgorithmsGreedy/src/CuttingBoards.java
@@ -13,28 +13,28 @@ public class CuttingBoards {
 
 			int m = in.nextInt();
 			int n = in.nextInt();
-			int[] arrX = new int[m - 1];
-			int[] arrY = new int[n - 1];
+			long[] arrX = new long[m - 1];
+			long[] arrY = new long[n - 1];
 
 			for (int i = 0; i < m - 1; i++) {
-				arrX[i] = in.nextInt();
+				arrX[i] = in.nextLong();
 			}
 
 			for (int i = 0; i < n - 1; i++) {
-				arrY[i] = in.nextInt();
+				arrY[i] = in.nextLong();
 			}
 
 			Arrays.sort(arrX);
 			Arrays.sort(arrY);
 
 			for (int i = 0; i < (m - 1) / 2; i++) {
-				int temp = arrX[i];
+				long temp = arrX[i];
 				arrX[i] = arrX[m - 2 - i];
 				arrX[m - 2 - i] = temp;
 			}
 
 			for (int i = 0; i < (n - 1) / 2; i++) {
-				int temp = arrY[i];
+				long temp = arrY[i];
 				arrY[i] = arrY[n - 2 - i];
 				arrY[n - 2 - i] = temp;
 			}
@@ -50,34 +50,28 @@ public class CuttingBoards {
 
 				if (idx != arrX.length && idy == arrY.length) {
 					totalCost += arrX[idx] * numYPlates;
-					totalCost %= ((long) Math.pow(10, 9) + 7);
 					numXPlates++;
 					idx++;
 				} else if (idy != arrY.length && idx == arrX.length) {
 					totalCost += arrY[idy] * numXPlates;
-					totalCost %= ((long) Math.pow(10, 9) + 7);
 					numYPlates++;
 					idy++;
 				} else if (idy != arrY.length && idx != arrX.length) {
 					if (arrX[idx] > arrY[idy]) {
 						totalCost += arrX[idx] * numYPlates;
-						totalCost %= ((long) Math.pow(10, 9) + 7);
 						numXPlates++;
 						idx++;
 					} else if (arrX[idx] < arrY[idy]) {
 						totalCost += arrY[idy] * numXPlates;
-						totalCost %= ((long) Math.pow(10, 9) + 7);
 						numYPlates++;
 						idy++;
 					} else {
 						if ((arrX[idx] * numYPlates) < (arrY[idy] * numXPlates)) {
 							totalCost += arrX[idx] * numYPlates;
-							totalCost %= ((long) Math.pow(10, 9) + 7);
 							numXPlates++;
 							idx++;
 						} else {
 							totalCost += arrY[idy] * numXPlates;
-							totalCost %= ((long) Math.pow(10, 9) + 7);
 							numYPlates++;
 							idy++;
 						}
@@ -85,7 +79,8 @@ public class CuttingBoards {
 				}
 
 			}
-			System.out.println(totalCost % ((long) Math.pow(10, 9) + 7));
+
+			System.out.println((long) (totalCost % (Math.pow(10, 9) + 7)));
 
 			T--;
 		}
