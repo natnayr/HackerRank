b6a7ab3fa8ff139f2a827711a3929b78331e5f83
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2016-01-27 18:09:44 +0800

    SansaAndXOR works Cant solve Cipher without understanding editorial (failure)

--- AlgorithmsBitManipulation/src/SansaAndXOR.java
+++ AlgorithmsBitManipulation/src/SansaAndXOR.java
@@ -1,8 +1,59 @@
+import java.util.Scanner;
 
 public class SansaAndXOR {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+
+		int T = in.nextInt();
+
+		while (T > 0) {
+
+			int N = in.nextInt();
+
+			int[] arr = new int[N];
+
+			for (int i = 0; i < N; i++) {
+				arr[i] = in.nextInt();
+			}
+
+			if ((N % 2) == 0) {
+				System.out.println(0);
+			} else {
+
+				int total = 0;
+
+				// for (int size = 1; size <= N; size++) {
+				// // System.out.println("size:" + size);
+				// for (int start = 0, end = size - 1; end < N; start++, end++)
+				// {
+				//
+				// int subSqn = 0;
+				// // System.out.println("start:" + start + " end:" + end);
+				//
+				// for (int i = start; i <= end; i++) {
+				//
+				// // System.out.println("index:" + i);
+				// if (arr[i] != 0) {
+				// subSqn ^= arr[i];
+				// }
+				// }
+				// if (subSqn != 0) {
+				// total ^= subSqn;
+				// }
+				// }
+				// }
+
+				for (int i = 0; i < N; i += 2) {
+					total ^= arr[i];
+				}
+
+				System.out.println(total);
+			}
+			T--;
+		}
+
+		in.close();
 
 	}
 
