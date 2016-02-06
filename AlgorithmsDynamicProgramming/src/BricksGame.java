2f8103b7b926fcbe7a1296f78d5c713c809f2421
Ryan Tan <Ryan@Ryans-MacBook-Pro.local>
2016-02-07 02:19:08 +0800

    Really can't solve....

--- AlgorithmsDynamicProgramming/src/BricksGame.java
+++ AlgorithmsDynamicProgramming/src/BricksGame.java
@@ -2,63 +2,48 @@ import java.util.Scanner;
 
 public class BricksGame {
 
+	public int[] sum;
+	public int[] dp;
+	public int[] arr;
+
 	public BricksGame() {
 	}
 
 	public static void main(String[] args) {
-		BricksGame bg = new BricksGame();
+
 		Scanner in = new Scanner(System.in);
 
 		int T = in.nextInt();
 		while (T > 0) {
-
+			BricksGame bg = new BricksGame();
 			int N = in.nextInt();
-			int[] arr = new int[N];
+
+			bg.arr = new int[N];
+			bg.sum = new int[N];
+			bg.dp = new int[N + 1];
 
 			for (int i = 0; i < N; i++) {
-				arr[i] = in.nextInt();
+				bg.arr[i] = in.nextInt();
+				bg.dp[i] = 0;
 			}
 
-			int score = bg.pickBricks(arr, 0, 1, 0);
+			bg.sum[0] = bg.arr[0];
+			for (int i = 1; i < N; i++) {
+				bg.sum[i] = bg.arr[i] + bg.sum[i - 1];
+			}
 
-			System.out.println(score);
+			bg.dp[0] = 0;
+			bg.dp[1] = ;
+			bg.dp[2] = bg.sum[2];
+
+			for (int i = 3; i <= N; i++) {
+				
+			}
 
 			T--;
 		}
 
 		in.close();
-
-	}
-
-	// 1 is my move, 2 is other
-	public int pickBricks(int[] arr, int idx, int turn, int myScore) {
-
-		// 3 or less bricks left, just return score
-		if ((arr.length - idx) <= 3) {
-			// calculate remaining score if my turn
-			if (turn == 1) {
-				for (int i = idx; i < arr.length; i++) {
-					myScore += arr[i];
-				}
-			}
-			return myScore;
-		}
-
-		// swap turns
-		if (turn == 1) {
-			turn = 2;
-			int myScore1 = myScore + arr[idx];
-			int myScore2 = myScore + arr[idx] + arr[idx + 1];
-			int myScore3 = myScore + arr[idx] + arr[idx + 1] + arr[idx + 2];
-			return Math.max(pickBricks(arr, idx + 1, turn, myScore1),
-					Math.max(pickBricks(arr, idx + 2, turn, myScore2), pickBricks(arr, idx + 3, turn, myScore3)));
-		} else {
-			turn = 1;
-			return Math.max(pickBricks(arr, idx + 1, turn, myScore),
-					Math.max(pickBricks(arr, idx + 2, turn, myScore), pickBricks(arr, idx + 3, turn, myScore)));
-			// return pickBricks(arr, idx + 3, turn, myScore);
-
-		}
 
 	}
 
